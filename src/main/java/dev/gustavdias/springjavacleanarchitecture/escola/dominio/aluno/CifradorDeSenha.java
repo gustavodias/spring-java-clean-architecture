2ead578ec6a3dc1a9302b650c4a5317f8436648c
Gustavo <gustavoemersonfd@gmail.com>
2022-07-21 13:08:48 -0300

    Add CifradorDeSenha in Aluno domain(business rule)

---
+++ src/main/java/dev/gustavdias/springjavacleanarchitecture/escola/dominio/aluno/CifradorDeSenha.java
@@ -0,0 +1,7 @@
+package dev.gustavdias.springjavacleanarchitecture.escola.dominio.aluno;
+
+public interface CifradorDeSenha {
+    String cifrarSenha(String senha);
+
+    boolean validarSenhaCifrada(String senhaCifrada, String senha);
+}
